e07e8ad6b3818f8fea3715db5362df1cf1ec90da
naren <naren.yellavula@knowlarity.com>
2016-06-21 00:56:58 +0530

    added table migration file

---
+++ migrate.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:passapp@tcp(127.0.0.1:3306)/gotest")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	stmt, err := db.Prepare("CREATE TABLE person (id int NOT NULL AUTO_INCREMENT, first_name varchar(40), last_name varchar(40), PRIMARY KEY (id));")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		fmt.Print(err.Error())
+	} else {
+		fmt.Printf("Person Table successfully migrated....")
+	}
+}
